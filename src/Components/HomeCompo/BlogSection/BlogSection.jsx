3b38d14303fb4831787191c6ff671868e5d5efed
iftekharr001 <iftekharr001@gmail.com>
2023-11-05 11:27:55 -0800

    added blog section in home page

---
+++ src/Components/HomeCompo/BlogSection/BlogSection.jsx
@@ -0,0 +1,88 @@
+
+const BlogSection = () => {
+    return (
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="font-bold text-5xl mb-6">
+            OUR{" "}
+            <span className="bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text">
+              BLOG
+            </span>
+          </h1>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have <br /> suffered lebmid alteration in some ledmid
+            form
+          </p>
+          <div className="w-[1140px] mx-auto grid grid-cols-3 gap-6 mt-16">
+            <div className=" border-[5px]">
+              <div className="card  bg-base-100 shadow-xl">
+                <div className="card-body text-left">
+                  <h2 className="card-title mb-4 bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text">
+                    Signficant Reading Has A More Info Number
+                  </h2>
+                  <p className="mb-2">
+                    Significant reading has a more in-depth focus on
+                    understanding and exploring a subject. It delves into the
+                    details, offering a deeper comprehension of the topic. This
+                    approach aids in gaining valuable insights and knowledge,
+                    making it an essential practice for comprehensive learning
+                    and informed decision-making.
+                  </p>
+                  <hr />
+                  <p className="mt-2 bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text font-bold">
+                    Jul 22, 18
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="  border-[5px]">
+              <div className="card  bg-base-100 shadow-xl">
+                <div className="card-body text-left">
+                  <h2 className="card-title mb-4 bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text">
+                    Signficant Reading Has A More Info Number
+                  </h2>
+                  <p className="mb-2">
+                    Significant reading has a more in-depth focus on
+                    understanding and exploring a subject. It delves into the
+                    details, offering a deeper comprehension of the topic. This
+                    approach aids in gaining valuable insights and knowledge,
+                    making it an essential practice for comprehensive learning
+                    and informed decision-making.
+                  </p>
+                  <hr />
+                  <p className="mt-2 bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text font-bold">
+                    Jul 22, 18
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className=" border-[5px]">
+              <div className="card  bg-base-100 shadow-xl">
+                <div className="card-body text-left">
+                  <h2 className="card-title mb-4 bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text">
+                    Signficant Reading Has A More Info Number
+                  </h2>
+                  <p className="mb-2">
+                    Significant reading has a more in-depth focus on
+                    understanding and exploring a subject. It delves into the
+                    details, offering a deeper comprehension of the topic. This
+                    approach aids in gaining valuable insights and knowledge,
+                    making it an essential practice for comprehensive learning
+                    and informed decision-making.
+                  </p>
+                  <hr />
+                  <p className="mt-2 bg-gradient-to-r from-[#3873b6]  to-[#5d9ee2] inline-block text-transparent bg-clip-text font-bold">
+                    Jul 22, 18
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default BlogSection;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import BlogSection from "../../Components/HomeCompo/BlogSection/BlogSection";
 import Hero from "../../Components/HomeCompo/Hero/Hero";
 
 const Home = () => {
     return (
         <div>
-            <Hero></Hero>
+            <Hero></Hero>,
+            <BlogSection></BlogSection>
         </div>
     );
 };
